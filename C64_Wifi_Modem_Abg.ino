aea5f041fd1f99a5b0bedded785fdfdd100be47b
ageppert <ageppert@gmail.com>
2019-05-31 20:35:00 -0500

    Display one pixel on OLED!

--- C64_Wifi_Modem_Abg.ino
+++ C64_Wifi_Modem_Abg.ino
@@ -734,30 +734,30 @@ void setup() {
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   // Andy's test DIYMORE.CC
-//  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64 
-//    Serial.println(F("SSD1306 allocation failed"));
-//    for(;;); // Don't proceed, loop forever
-//  }
-//
-//  // Show initial display buffer contents on the screen --
-//  // the library initializes this with an Adafruit splash screen.
-//  display.display();
-//  delay(2000); // Pause for 2 seconds
-//
-//  // Clear the buffer
-//  display.clearDisplay();
-//
-//  // Draw a single pixel in white
-//  display.drawPixel(10, 10, WHITE);
-//
-//  // Show the display buffer on the screen. You MUST call display() after
-//  // drawing commands to make them visible on screen!
-//  display.display();
-//  delay(2000);
-//  // display.display() is NOT necessary after every single drawing command,
-//  // unless that's what you want...rather, you can batch up a bunch of
-//  // drawing operations and then update the screen all at once by calling
-//  // display.display(). These examples demonstrate both approaches...
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  // display.display() is NOT necessary after every single drawing command,
+  // unless that's what you want...rather, you can batch up a bunch of
+  // drawing operations and then update the screen all at once by calling
+  // display.display(). These examples demonstrate both approaches...
 //  testdrawline();      // Draw many lines
 //  testdrawrect();      // Draw rectangles (outlines)
 //  testfillrect();      // Draw rectangles (filled)
